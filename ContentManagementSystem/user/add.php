5f077b81405607c2a655c81b9f800b03dbc8be11
leo5916267 <leo5916267@gmail.com>
2016-10-31 15:47:56 +0800

    時間改為24改變add路徑

---
+++ ContentManagementSystem/user/add.php
@@ -0,0 +1,39 @@
+<?php
+      ini_set("display_errors",'On');
+
+      $webSite   = $_POST["webSite"];
+      $fileName  = $_FILES['file']['name'];
+      //檔案記得表單需加(enctype='multipart/form-data')
+      $startDate = $_POST["startDate"];
+      $endDate =   $_POST["endDate"];
+      $appearTime = $_POST["appearTime"];
+
+      if($_FILES["file"]["error"]==0){
+          move_uploaded_file($_FILES["file"]["tmp_name"],
+          iconv("UTF-8", "big5", "../picture/".$_FILES["file"]["name"] ));//防止中文檔名亂碼
+      }else {
+          echo"fileErrorCode:".$_FILES["file"]["error"];
+      }
+
+
+      include("../method/connectManage.php");
+
+      $insert = $connectManage -> prepare( "INSERT INTO
+                          unviewed (
+                                  webSite,
+                                  fileName,
+                                  startDate,
+                                  endDate,
+                                  appearTime
+                                ) VALUES (
+                                  ?,?,?,?,?
+                                )");
+      $insert -> execute(
+              array( $webSite,
+                           $fileName,
+                           $startDate,
+                           $endDate,
+                           $appearTime
+                         ));
+      header("location:http://localhost/ContentManagementSystem/manage/unviewed/index.php");
+ ?>
